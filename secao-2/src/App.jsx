d544cbdf398fc085891b534e3ebb5fd2abfb7db1
Diego Brito <diegorbrito9@gmail.com>
2022-07-16 16:24:15 -0300

    secao 2: fim da aula state com arrays e objetos

--- secao-2/src/App.jsx
+++ secao-2/src/App.jsx
@@ -4,24 +4,39 @@ export class App extends Component {
   constructor() {
     super();
     this.state = {
-      count: 0,
+      posts: [
+        { id: 1, title: 'Titulo 1', content: 'daisdoiahsdoiahosdhi' },
+        { id: 2, title: 'Titulo 2', content: 'daisdoiahsdoiahosdhi' },
+        { id: 3, title: 'Titulo 3', content: 'daisdoiahsdoiahosdhi' },
+      ],
     };
   }
 
+  addNewPost = () => {
+    const { posts } = this.state;
+    const id = posts[posts.length - 1].id + 1;
+
+    const newPost = { id, title: `Titulo ${id}`, content: 'daisdoiahsdoiahosdhi' };
+    this.setState({ posts: [...posts, newPost] });
+  };
+
   render() {
-    const { count } = this.state;
-
+    const { posts } = this.state;
     return (
-      <div>
-        <p>{count}</p>
+      <>
+        {
+          posts.map((post) => (
+            <div key={post.id}>
+              <h1>{post.title}</h1>
+              <p>{post.content}</p>
+            </div>
+          ))
+        }
 
-        <button
-          onClick={() => this.setState({ count: count + 1 })}
-          type="button"
-        >
-          Increment
+        <button type="button" onClick={this.addNewPost}>
+          Add new post
         </button>
-      </div>
+      </>
     );
   }
 }
