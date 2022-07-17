5d69dab040001a92c97fe70a2bc47bafeedb8cfd
Diego Brito <diegorbrito9@gmail.com>
2022-07-17 12:53:31 -0300

    new: criando rotas com next

--- secao-2-react-basico/src/App.jsx
+++ secao-2-react-basico/src/App.jsx
@@ -1,38 +1,15 @@
 import React, { Component } from 'react';
-import './style/App.css';
-import getPosts from './service/getPosts';
-import getPhotos from './service/getPhotos';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Teste from './pages/Teste';
 
-export class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      posts: [],
-    };
-  }
-
-  async componentDidMount() {
-    const [allPosts, photos] = await Promise.all([getPosts(), getPhotos()]);
-
-    const posts = allPosts.map((post, index) => ({ ...post, image: photos[index].thumbnailUrl }));
-
-    this.setState({ posts });
-  }
-
+class App extends Component {
   render() {
-    const { posts } = this.state;
-
     return (
-      <div className="container-card">
-        {
-          posts.length > 1 && posts.map((post) => (
-            <div key={post.id} className="card">
-              <h1 style={{ color: 'black' }}>{post.title}</h1>
-              <img src={post.image} alt="thumbnail" />
-            </div>
-          ))
-        }
-      </div>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/teste" element={<Teste />} />
+      </Routes>
     );
   }
 }
